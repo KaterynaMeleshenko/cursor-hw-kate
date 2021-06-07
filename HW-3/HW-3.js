f6b731fc938f67018fe3b8d30647f574fe184eda
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-06-08 00:22:27 +0300

    added 2 functions

--- HW-3/HW-3.js
+++ HW-3/HW-3.js
@@ -6,30 +6,63 @@ function getMaxDigit(number) {
     let arrayNumbers = split.map(Number);      // перевожу символы в числа
     let maxNumber = Math.max(...arrayNumbers); // массив превращаю в аргументы и ищу максимальное значение из
     return maxNumber;
-  }
+}
  
-  document.write(`Function No 1: ${getMaxDigit(14354456464)} <br>`);
+document.write(`Function No 1: ${getMaxDigit(14354456464)} <br>`);
   
-  // 2st function
+// 2st function
   
-  function powerForNumber(num, pow) {
+function putNumberInPower(num, pow) {
     let result = 1;
-    for (let i = 1;  i <= pow; i++) {
+    if (pow > 0) {
+      for (let i = 1;  i <= pow; i++) {
       result *= num;
+      }
     }
+    else if (pow < 0) {
+      console.log('yes');
+      for (let i = 1;  i <= Math.abs(pow); i++) {
+      result =  (1 / num) * result;
+      }
+    }
+    else if (pow === 0) result = 1;
+
       return result;
   }
   
-  document.write(`Function No 2: ${powerForNumber(6, 4)} <br>`);
+document.write(`Function No 2: ${putNumberInPower(2, -3)} <br>`);
   
   
-  // 3rd function
+// 3rd function
   
-  function correctName(name) {
+function correctName(name) {
     let smallLetters = name.toLowerCase();
     let firstCapitalLetter =name[0].toUpperCase();
     let fullName = firstCapitalLetter + smallLetters.substring(1);
     return fullName;
-  }
+}
   
-  document.write(`Function No 3: ${correctName('kaTya')} <br>`);
+document.write(`Function No 3: ${correctName('kaTya')} <br>`);
+
+// 4th function
+
+function calculateRevenue(salary, rate) {
+  let taxesCoefficient = (1 - rate / 100);
+  let revenue = salary * taxesCoefficient;
+  return revenue;
+}
+
+document.write(`Function No 4: ${calculateRevenue(650, 19.5)} <br>`);
+
+// 5th function
+
+function getRandomNumber(n, m) {
+  let maxNumber = Math.floor( Math.max(n, m) );
+  let minNumber = Math.ceil( Math.min(n, m) );
+
+  // 1 added in order to include both min and max
+  let randomNumber = Math.random() * (maxNumber- minNumber + 1) + minNumber 
+  return Math.trunc(randomNumber);
+}
+
+  document.write(`Function No 4: ${getRandomNumber(-500, 400)} <br>`);
