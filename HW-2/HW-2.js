a0e598701efd926a33636874db2ad616beef654b
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-06-09 20:54:43 +0300

    refactured code

--- HW-2/HW-2.js
+++ HW-2/HW-2.js
@@ -1,64 +1,55 @@
-let firstNum;
-let secondNum;
-let evenSkip;
-
-let total;
+let firstNum = null;
+let secondNum = null;
 
 // verification of user's first тumber with alert in case of mistake and 
 // promt in cycle for a new try
 
-firstNum = prompt("Please, set the first integer number for calculation.", 0);
-
-while (isNaN(+firstNum) || firstNum === null || !Number.isInteger(+firstNum)) {
-    alert("It's not the required type of number! Please, try again.");
-	firstNum = +prompt("Please, set the first integer number for calculation.", 0);
-}
+do {firstNum = prompt("Please, set the first integer number for calculation.", 0)
+  }
+while (isNaN(+firstNum) || firstNum === null || !Number.isInteger(+firstNum));
 
 document.write(`Your first number is ${firstNum}.<br>`);
 
 // verification of user's second number with alert in case of mistake and 
 // promt in cycle for a new try
 
-secondNum = prompt("Please, set the second integer number for calculation.", 0);
-
+do {secondNum = prompt("Please, set the second integer number for calculation.", 0);
+}
 while (isNaN(+secondNum) || secondNum === null || !Number.isInteger(+secondNum)) {
-  alert("It's not the required type of number! Please, try again.");
-  secondNum = +prompt("Please, set the second integer number for calculation.", 0);
+  
 }
 
 document.write(`Your second number is ${secondNum}.<br>`);
 
 // to become sure that the initial number is less than the second number
 
-let initialNum = Math.min(firstNum,secondNum);
-let lastNum = Math.max(firstNum,secondNum);
+const initialNum = Math.min(firstNum,secondNum);
+const lastNum = Math.max(firstNum,secondNum);
 
 // ask user either they want to skip even numbers
 
-evenSkip = confirm("Do you want to skip even numbers in the calculation?");
+const evenSkip = confirm("Do you want to skip even numbers in the calculation?");
 
-total = 0;
+let total = 0;
 
 // main calculation given the confirmation about even numbers
 
-for (initialNum; initialNum <= lastNum; initialNum++) {
-  if (evenSkip && initialNum % 2 == 0) {
-    continue;
-    }
-  total += initialNum;
+for (let i = initialNum; i <= lastNum; i++) {
+  if (!evenSkip || i % 2 ) {
+  total += 1;
+  }
 }
 
 // set a virable for a conclusion
 
-let decision;
+let decision = "";
 
 if (evenSkip) {
   document.write(`You decided to skip even numbers.<br>`);
   decision = "excluding even numbers";
-}
-else {
+} else {
   document.write(`You decided not to skip even.<br>`);
   decision = "including even numbers";
   }
 
-document.write(`Total sum of all numbers between ${firstNum} and ${secondNum} (${decision}) is ${total}.`);
+document.write(`Total sum of all numbers between ${firstNum} and ${secondNum} (${decision}) is ${total}.`)
