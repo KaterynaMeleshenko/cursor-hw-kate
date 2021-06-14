d0971b83e1c6c51ebcd30c405363df091b8e0aac
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-06-14 19:57:11 +0300

    added 'and' to arrows

--- HW-4/HW-4.js
+++ HW-4/HW-4.js
@@ -6,9 +6,7 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 function getPairs(students) {
     const allPairs = [];
-    allPairs.push([students[0], students[2]]);
-    allPairs.push([students[1], students[3]]);
-    allPairs.push([students[4], students[5]]);
+    allPairs.push([students[0], students[2]], [students[1], students[3]], [students[4], students[5]]);
     return allPairs;
 }
 
@@ -19,7 +17,7 @@ console.log(getPairs(students));
 function setThemes(students, themes) {
     const allThemes = [];
     for (let i = 0; i < themes.length; i++) {
-        allThemes.push([getPairs(students)[i], themes[i]])
+        allThemes.push([getPairs(students)[i].join(' and '), themes[i]])
     }
     return allThemes;
 }
@@ -44,7 +42,7 @@ function setRandomMarks(students, themes) {
     const randomMarks = [];
     for (let i = 0; i < setThemes(students, themes).length; i++) {
         const mark = Math.trunc(Math.random()*(5-1+1)+1);
-        randomMarks.push([setThemes(students, themes)[i], mark]);
+        randomMarks.push([setThemes(students, themes)[i].concat(mark)]);
     }
     return randomMarks;
 }
