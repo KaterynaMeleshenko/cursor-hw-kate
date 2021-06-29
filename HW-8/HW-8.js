95f5b4ec9563ab80e8c02d3256862ea07eadfa92
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-06-29 03:00:48 +0300

    advanced tasks uploaded

--- HW-8/HW-8.js
+++ HW-8/HW-8.js
@@ -1,5 +1,6 @@
 // BASE
 
+// 1
 class Student {
   constructor(university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies') {
     this.university = university;
@@ -54,20 +55,26 @@ class Student {
 
 const ostap = new Student("Hight school of the psychteraphy, Odessa", "first", "Ostap Rodomanskiy Bender");
 
+// 2
 console.log('Get info:', ostap.getInfo());
 
+// 3
 console.log('Get marks:', ostap.getMarks);
 
+// 4
 ostap.setMarks = 5;
 console.log('Get new marks:', ostap.getMarks);
 
+// 5
 console.log('Avarage mark is', ostap.getAverageMark());
 
+// 6
 console.log('* * * Dissmissing a student and attempt to put him a mark * * *')
 console.log(ostap.dismiss());
 ostap.setMarks = 4;
 console.log(ostap.getMarks); 
 
+// 7
 console.log('* * * Recovering a student and attempt to put him a mark * * *')
 console.log(ostap.recover());
 ostap.setMarks = 3;
@@ -75,3 +82,34 @@ console.log(ostap.getMarks);
 
 
 // ADVANCED
+
+console.log('* * * Advanced * * *')
+
+class BudgetStudent extends Student {
+  constructor (
+    university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies', superStatus = 'budget', 
+    scholarship = 1400) {
+      super (university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies')
+  
+      this.superStatus = superStatus;
+      this.scholarship = scholarship;
+      setInterval(() => this.getScholarship(), 10000);
+    }
+      
+  getScholarship() {
+    if (this.superStatus === 'budget' & this.getAverageMark() >= 4 & this.status !== 'expelled') {
+      console.log(`${this.fullName} received ${this.scholarship} grn of scholarship`)
+    } else if (this.superStatus === 'budget' & this.getAverageMark() < 4 & this.status !== 'expelled') {
+      console.log(`${this.fullName} can't receive their scholarship as their average mark is ${this.getAverageMark()} 
+        that is less than 4`)
+    } else if (this.status === 'expelled') {
+      console.log(`${this.fullName} doesn't receive their scholarship anymore as they are expelled`);
+    }
+  }
+}
+
+const kate = new BudgetStudent('Odessa National Maritime University', 'fifth', 'Meleshenko Ekaterina');
+const anton = new BudgetStudent('Odessa National Economic University', 'forth', 'Vuchkan Anton');
+anton.allMarks = [2, 3, 4, 3];
+const bogdan = new BudgetStudent('Odessa National Economic University', 'third', 'Bogdan Chop');
+bogdan.status = 'expelled';
