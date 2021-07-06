41c326d83cb0958c456422f916508695a4109e44
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-07-06 18:51:46 +0300

    fixed bugs

--- HW-10/HW-10.js
+++ HW-10/HW-10.js
@@ -1,16 +1,37 @@
 const music = ['m', 'u', 's', 'i', 'c'];
 
-music.forEach(letter => {
+function activateSound(letter) {
+  deactivateAll();
+  document.getElementById(letter).classList.add("active");
   const myMusic = document.getElementById(letter+'Music');
+  myMusic.play();
+}
 
-  document.addEventListener('keydown', function (e) {
-    if (e.key === letter) {
-      ( document.getElementById(letter) ).classList.add("active");
-      myMusic.play();
-     } else {
-        ( document.getElementById(letter) ).classList.remove("active");
-     }
-  });
-})
+function deactivateAll() {
+  music.forEach(letter => {
+    document.getElementById(letter).classList.remove("active");
+    const myMusic = document.getElementById(letter+'Music');
+    myMusic.pause();
+    myMusic.currentTime = 0;
+  })
+} 
 
+function letShowGoOn() {
+  music.forEach(letter => {
+    
+    document.addEventListener('keydown', function (e) {
+      if (e.key === letter) {
+      activateSound(letter);
+      } else {
+       document.getElementById(letter).classList.remove("active");
+      }
+    });
+  
+    document.getElementById(letter).onclick = function() {
+     activateSound(letter);
+    }
+  })
+}
+
+letShowGoOn();
 
