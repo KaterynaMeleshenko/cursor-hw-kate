74e977a2ea89407f61d154487c35231a0730dadb
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-07-19 00:27:13 +0300

    8th HW refactoring

--- HW-8/HW-8.js
+++ HW-8/HW-8.js
@@ -2,7 +2,7 @@
 
 // 1
 class Student {
-  constructor(university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies') {
+  constructor(university, course, fullName, allMarks, status) {
     this.university = university;
     this.course = course;
     this.fullName = fullName;
@@ -11,7 +11,7 @@ class Student {
   }
 
   getInfo() {
-     if (this.status == 'studies' || this.status == 'recovered') {
+     if (this.status === true) {
      return `Student of the ${this.course} course of ${this.university}, ${this.fullName}`;
      } else {
          return "This student is not studying here anymore";
@@ -19,7 +19,7 @@ class Student {
   };
 
   get getMarks() {
-    if (this.status == 'studies' || this.status == 'recovered') {
+    if (this.status === true) {
       return this.allMarks;
     } else {
       return null;
@@ -27,8 +27,8 @@ class Student {
   };
 
   set setMarks(mark) {
-    if (this.status == 'studies' || this.status == 'recovered') {
-      return this.allMarks.push(mark);
+    if (this.status === true) {
+      return this.allMarks = [...this.allMarks, mark];
     }
   }
 
@@ -41,19 +41,19 @@ class Student {
     }
 
   dismiss() {
-    this.status = 'expelled';
+    this.status = false;
 
     return this.getInfo();
   };
 
   recover() {
-    this.status = 'recovered';
+    this.status = true;
 
     return `The student ${this.fullName} is recovered`;   
   };
 }
 
-const ostap = new Student("Hight school of the psychteraphy, Odessa", "first", "Ostap Rodomanskiy Bender");
+const ostap = new Student("Hight school of the psychteraphy, Odessa", "first", "Ostap Rodomanskiy Bender", [5, 4, 4, 5], true);
 
 // 2
 console.log('Get info:', ostap.getInfo());
@@ -87,29 +87,27 @@ console.log('* * * Advanced * * *')
 
 class BudgetStudent extends Student {
   constructor (
-    university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies', superStatus = 'budget', 
-    scholarship = 1400) {
-      super (university, course, fullName, allMarks = [5, 4, 4, 5], status = 'studies')
+    university, course, fullName, allMarks, status, superStatus, scholarship) {
+      super (university, course, fullName, allMarks, status)
   
       this.superStatus = superStatus;
       this.scholarship = scholarship;
-      setInterval(() => this.getScholarship(), 10000);
+      setInterval(() => this.getScholarship(), 5000);
     }
       
   getScholarship() {
-    if (this.superStatus === 'budget' & this.getAverageMark() >= 4 & this.status !== 'expelled') {
+    if (this.superStatus === true & this.getAverageMark() >= 4 & this.status === true) {
       console.log(`${this.fullName} received ${this.scholarship} grn of scholarship`)
-    } else if (this.superStatus === 'budget' & this.getAverageMark() < 4 & this.status !== 'expelled') {
+    } else if (this.superStatus === true & this.getAverageMark() < 4 & this.status === true) {
       console.log(`${this.fullName} can't receive their scholarship as their average mark is ${this.getAverageMark()} 
         that is less than 4`)
-    } else if (this.status === 'expelled') {
+    } else if (this.status === false) {
       console.log(`${this.fullName} doesn't receive their scholarship anymore as they are expelled`);
     }
   }
 }
 
-const kate = new BudgetStudent('Odessa National Maritime University', 'fifth', 'Meleshenko Ekaterina');
-const anton = new BudgetStudent('Odessa National Economic University', 'forth', 'Vuchkan Anton');
-anton.allMarks = [2, 3, 4, 3];
-const bogdan = new BudgetStudent('Odessa National Economic University', 'third', 'Bogdan Chop');
-bogdan.status = 'expelled';
+const kate = new BudgetStudent('Odessa National Maritime University', 'fifth', 'Meleshenko Ekaterina',
+  [5, 4, 4, 5], true, true, 1400);
+const anton = new BudgetStudent('Odessa National Economic University', 'forth', 'Vuchkan Anton', [2, 3, 4, 3], true, true, 1400);
+const bogdan = new BudgetStudent('Odessa National Economic University', 'third', 'Bogdan Chop', [5, 4, 4, 5], false, true, 1400);
