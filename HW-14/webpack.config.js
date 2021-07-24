ce2ffff84a152ae02f921fd505010c3124f4cf94
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-07-25 00:30:40 +0300

    initial upload

---
+++ HW-14/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "styles.css"
+})],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ]
+    },
+  
+};
