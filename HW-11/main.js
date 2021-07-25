d24a5d498bc5ee27f90f50b3626e339d6eb59433
KaterynaMeleshenko <yekaterynamel@gmail.com>
2021-07-25 23:14:33 +0300

    refactoring

--- HW-11/main.js
+++ HW-11/main.js
@@ -2,22 +2,21 @@ const length = prompt('Please press the quantity of Chineese characters');
 
 function getCharacter() {
   const now = Date.now().toString();
-  const sign = + now.slice(now.length-5, now.length);
+  const sign = + now.slice(now.length - 5, now.length);
   const character = String.fromCharCode(sign);
+
   return character;
 }
 
 async function getRandomChinese(length) {
   let chineseCharacters = '';
-  let i = 0;
     
-  while (i < length) {
+  for (let i = 0; i < length; i++) {
     const promise = new Promise (function(resolve, reject) {
-      setTimeout(() => resolve(getCharacter()), 1000);
-      i++;
+      setTimeout(() => resolve(getCharacter()), 50);
     });
 
-    chineseCharacters += await promise;
+  chineseCharacters += await promise;
   }
 
   return chineseCharacters;
@@ -26,7 +25,7 @@ async function getRandomChinese(length) {
 const promise = getRandomChinese(length);
 
 promise.then(
-result => console.log(result)
+  result => console.log(result)
 ).catch (error => {
-    console.log(`It's unable to find chineese characters`)
+  console.log(`It's unable to find chineese characters`);
 })
